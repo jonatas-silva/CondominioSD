15d917046a7616aac95076c327c3e46e228cac3d
jonatasoliveira <49698783+jonatas-silva@users.noreply.github.com>
2019-06-16 23:56:17 -0300

    Add files via upload

--- constCS.py
+++ constCS.py
@@ -1,13 +1,24 @@
+# bibliotecas utilizadas
+
+import zmq
+import ast # biblioteca usada para converter String em um Dicionario
+from random import * # randint(0,9) , random.choice('abc') # Gera numeros aleatorios inteiros
+import time
+
 #REDE
 
-HOST  = "192.168.0.73" # IP do HOST local
-HOST2  = "54.152.78.245" # IP publico da maquina na AWS [server_andar]
+HOST  = "192.168.0.73" # ip privado local
+HOST2  = "54.167.196.182" # ip publico AWS
 PORT1 = "5555"
 PORT2 = "5556"
+PORT3 = "5557"
+PORT4 = "5558"
+PORT5 = "5559"
+# PORT6 = "5560"
 
 # PREDIOS
 
-predio1 = "192.168.0.73"
+predio1 = "192.168.0.73" # ip privado local
 # predio1_MAX = 50
 
 predio2 = "192.168.0.75"
@@ -21,3 +32,10 @@ predio4 = "192.168.0.77"
 
 # ANDARES
 
+andar1 = "192.165.0.5"
+
+andar2 = "192.165.0.5"
+
+andar3 = "192.165.0.5"
+
+andar4 = "192.165.0.5"
--- constCS2.py
+++ constCS2.py
@@ -1,6 +1,6 @@
 #REDE
 
-HOST  = "172.31.43.122" # IP privado da AWS [server_andar]
+HOST  = "172.31.43.122"
 PORT1 = "5555"
 PORT2 = "5556"
 
@@ -8,19 +8,15 @@ PORT2 = "5556"
 
 predio1 = "192.168.0.73"
 predio1_MAX = 50
-predio1_qtd = 0
 
 predio2 = "192.168.0.75"
 predio2_MAX = 50
-predio2_qtd = 0
 
 predio3 = "192.168.0.76"
 predio3_MAX = 50
-predio3_qtd = 0
 
 predio4 = "192.168.0.77"
 predio4_MAX = 50
-predio4_qtd = 0
 
 # ANDARES
 
